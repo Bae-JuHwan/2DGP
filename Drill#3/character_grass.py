9bb037aa4601088bb622dc2fab8d5dcb4373e75b
juhwanTV <0412juhwan@gmail.com>
2024-09-24 12:18:56 +0900

    run_circle 함수

--- Drill#3/character_grass.py
+++ Drill#3/character_grass.py
@@ -4,49 +4,38 @@ import math
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+boy = load_image('character.png')
 
-x = 400
-y = 90
-center_x = 400
-center_y = 300
-radius = 200
-angle = 0
+def run_circle():
+    r, cx, cy = 300, 800 // 2, 600 // 2
 
-is_square_motion = True
+    for degree in range(0, 360, 3):
+        theta = math.radians(degree)
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+        
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.1)
+def run_rectangle():
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
 
-while(True):
-    clear_canvas_now()
-    grass.draw_now(400,30)
+def run_top():
+    pass
+def run_right():
+    pass
+def run_bottom():
+    pass
+def run_top():
+    pass
 
-    if is_square_motion:
-        character.draw_now(x, y)
-
-        if y == 90 and x < 790:
-            x += 2
-        elif x == 790 and y < 570:
-            y += 2
-        elif y == 570 and x > 10:
-            x -= 2
-        elif x == 10 and y > 90:
-            y -= 2
-
-        if x == 400 and y == 90:
-            is_square_motion = False
-            angle = 0
-
-    else:
-        x = center_x + radius * math.cos(angle)
-        y = center_y + radius * math.sin(angle)
-        character.draw_now(x, y)
-
-        angle += 0.05
-
-        if angle >= 2 * math.pi:
-            is_square_motion = True
-            x = 400
-            y = 90
-
+while True:
+    run_circle()
+    run_rectangle()
     delay(0.01)
 
 close_canvas()
+
