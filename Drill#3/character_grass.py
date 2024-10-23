a0a4cc5dc599441b5ddd4a62f27aeac944a7254c
juhwanTV <0412juhwan@gmail.com>
2024-10-23 13:15:42 +0900

    시험공부하면서 수정

--- Drill#3/character_grass.py
+++ Drill#3/character_grass.py
@@ -6,9 +6,10 @@ grass = load_image('grass.png')
 boy = load_image('character.png')
 
 def draw_boy(x,y):
-    clear_canvas_now()
+    clear_canvas()
     grass.draw_now(400, 30)
-    boy.draw_now(x,y)
+    boy.draw(x,y)
+    update_canvas()
     delay(0.01)
 
 def run_circle():
@@ -29,8 +30,8 @@ def run_rectangle():
 
 def run_triangle():
     run_bottom()
-    run_diagonalLeft()
-    run_diagonalRight()
+    run_left()
+    run_diagonal()
 
 def run_top():
     for x in range(20, 780, 10):
@@ -48,20 +49,13 @@ def run_left():
     for y in range(90, 550, 10):
         draw_boy(20, y)
 
-def run_diagonalLeft():
-    for r in range(0, 600, 10):
-        cx, cy = 20, 90
-        theta = 70
-        x = r * math.cos(theta) + cx
-        y = r * math.sin(theta) + cy
-        draw_boy(x,y)
-
-def run_diagonalRight():
-    for r in range(10, 600, 10):
-        cx, cy = 390,550
-        theta = 200
-        x = r * math.cos(theta) + cx
-        y = r * math.sin(theta) + cy
+def run_diagonal():
+    x1, y1 = 0, 600
+    x2, y2 = 800, 90
+    for i in range(0, 100 + 1, 4):
+        t = i / 100
+        x = (1-t)*x1 + t*x2
+        y = (1-t)*y1 + t*y2
         draw_boy(x,y)
 
 while True:
