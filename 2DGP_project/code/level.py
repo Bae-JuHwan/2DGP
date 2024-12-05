5d455c12b6c85849ad28042e3532595b687b754e
juhwanTV <0412juhwan@gmail.com>
2024-12-05 12:49:03 +0900

    식물 성장

--- 2DGP_project/code/level.py
+++ 2DGP_project/code/level.py
@@ -17,7 +17,7 @@ class Level:
 		self.tree_sprites = pygame.sprite.Group()
 		self.interaction_sprites = pygame.sprite.Group()
 
-		self.soil_layer = SoilLayer(self.all_sprites)
+		self.soil_layer = SoilLayer(self.all_sprites, self.collision_sprites)
 		self.setup()
 		self.overlay = Overlay(self.player)
 		self.transition = Transition(self.reset, self.player)
@@ -86,6 +86,9 @@ class Level:
 		self.player.item_inventory[item] += 1
 
 	def reset(self):
+		# plants
+		self.soil_layer.update_plants()
+
 		#soil
 		self.soil_layer.remove_water()
 
@@ -102,6 +105,7 @@ class Level:
 
 		self.overlay.display()
 
+		# transition overlay
 		if self.player.sleep:
 			self.transition.play()
 
